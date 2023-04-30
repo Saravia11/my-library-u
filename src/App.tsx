a53122b7b799b0560038758881cce404e26d8656
Saravia11 <diego.saravia@focusservices.com>
2023-04-29 19:18:37 -0600

    feat: adding librarian dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
 import Student from "./pages/Student";
+import Librarian from "./pages/Librarian";
 
 const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   { path: "/student", element: <Student /> },
+  { path: "/librarian", element: <Librarian /> },
 ]);
 
 const App = () => <RouterProvider router={router} />;
