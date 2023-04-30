f85a9c52f200e17b2009b2aa8992a5b07f673e62
Saravia11 <diego.saravia@focusservices.com>
2023-04-29 20:47:49 -0600

    feat: installing react query and dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {
   redirect,
   RouterProvider,
 } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Login from "./pages/Login";
 import Student from "./pages/Student";
 import Librarian from "./pages/Librarian";
@@ -16,6 +18,12 @@ const router = createBrowserRouter([
   { path: "/librarian", element: <Librarian /> },
 ]);
 
-const App = () => <RouterProvider router={router} />;
+const queryClient = new QueryClient();
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
 
 export default App;
