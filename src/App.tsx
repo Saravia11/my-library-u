a7740af53c8d9f14f34e0705a7e719d355b18dbd
Saravia11 <diego.saravia@focusservices.com>
2023-04-29 19:37:16 -0600

    feat: installing package for toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Login from "./pages/Login";
 import Student from "./pages/Student";
 import Librarian from "./pages/Librarian";
 
+import "react-toastify/dist/ReactToastify.css";
+
 const router = createBrowserRouter([
   { path: "/", loader: () => redirect("/login") },
   { path: "/login", element: <Login /> },
