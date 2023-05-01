2c88c18d535cdc84de52b674ca89485498d849e6
Saravia11 <diego.saravia@focusservices.com>
2023-05-01 01:53:49 -0600

    feat: using form to create new user

--- src/pages/Librarian.tsx
+++ src/pages/Librarian.tsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { Button } from "@mui/material";
 import BookFormDialog from "../components/BookFormDialog";
+import UserFormDialog from "../components/UserFormDialog";
 import BooksTable from "../components/BooksTable";
 import useBooks from "../hooks/useBooks";
 
 export default function Librarian() {
   const [openBookForm, setOpenBookForm] = useState(false);
-  const { refetch } = useBooks();
+  const [openUserForm, setOpenUserForm] = useState(false);
+  const { refetch: refetchBooks } = useBooks();
 
-  const handleCreationFinish = (success: boolean, message: string) => {
-    setOpenBookForm(false);
+  const handleCreationFinish = (
+    closeFormFn: (success: boolean) => void,
+    message: string,
+    success: boolean,
+    refetchFn: () => void
+  ) => {
+    closeFormFn(false);
     toast(message, {
       type: success ? "info" : "error",
     });
-    refetch();
+    refetchFn();
   };
 
   return (
@@ -22,11 +29,27 @@ export default function Librarian() {
       <Button variant="contained" onClick={() => setOpenBookForm(true)}>
         Add book
       </Button>
+      <Button
+        variant="contained"
+        color="success"
+        onClick={() => setOpenUserForm(true)}
+      >
+        Add user
+      </Button>
       <BooksTable />
       <BookFormDialog
         open={openBookForm}
         onClose={() => setOpenBookForm(false)}
-        onCreationFinish={handleCreationFinish}
+        onCreationFinish={(success, message) =>
+          handleCreationFinish(setOpenBookForm, message, success, refetchBooks)
+        }
+      />
+      <UserFormDialog
+        open={openUserForm}
+        onClose={() => setOpenUserForm(false)}
+        onCreationFinish={(success, message) =>
+          handleCreationFinish(setOpenUserForm, message, success, refetchBooks)
+        }
       />
       <ToastContainer hideProgressBar />
     </div>
