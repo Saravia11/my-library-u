663b38cf51c1d431c401eadfb64918bea7afbc0b
Saravia11 <diego.saravia@focusservices.com>
2023-05-01 13:24:01 -0600

    feat: showing boos and user info

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,8 +1,22 @@
 import Cookies from "js-cookie";
+import { ToastContainer, toast } from "react-toastify";
 import useUser from "../hooks/useUser";
+import BooksTable from "../components/BooksTable";
 
 export default function Student() {
-  const { user } = useUser(Cookies.get("user_id") || "");
+  const { user } = useUser(Cookies.get("user_id")!);
 
-  return <h1>{user?.name}</h1>;
+  const handleBookCheckout = (success: boolean, message: string) => {
+    toast(message, {
+      type: success ? "success" : "error",
+    });
+  };
+
+  return (
+    <div>
+      <h1>Hello {user?.name}</h1>
+      <BooksTable canOrder onCheckout={handleBookCheckout} />
+      <ToastContainer hideProgressBar />
+    </div>
+  );
 }
