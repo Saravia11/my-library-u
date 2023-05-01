0cc03617ab48084b0cb3078f101b3018096a8d1d
Saravia11 <diego.saravia@focusservices.com>
2023-05-01 13:20:52 -0600

    feat: validation for users role

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Cookies from "js-cookie";
 import Login from "./pages/Login";
 import Student from "./pages/Student";
 import Librarian from "./pages/Librarian";
@@ -14,8 +15,22 @@ import "react-toastify/dist/ReactToastify.css";
 const router = createBrowserRouter([
   { path: "/", loader: () => redirect("/login") },
   { path: "/login", element: <Login /> },
-  { path: "/student", element: <Student /> },
-  { path: "/librarian", element: <Librarian /> },
+  {
+    path: "/student",
+    element: <Student />,
+    loader: () => {
+      const role = Cookies.get("user_role");
+      return !role || role == "librarian" ? redirect("/librarian") : null;
+    },
+  },
+  {
+    path: "/librarian",
+    element: <Librarian />,
+    loader: () => {
+      const role = Cookies.get("user_role");
+      return !role || role == "student" ? redirect("/student") : null;
+    },
+  },
 ]);
 
 const queryClient = new QueryClient();
